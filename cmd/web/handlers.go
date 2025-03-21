df7bcd9dbdb2f48a778bf76da81088abba6eead4
faizisyellow <muhamatfaissalm@gmail.com>
2025-03-21 18:53:42 +0700

    implement loggout user

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -191,5 +191,11 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Logout the user"))
+	// Remove the userID from the session data so that the user is 'logged out'
+	app.session.Remove(r, "userID")
+
+	// Add a flash message to the session to confirm to the user that they've been logged out.
+	app.session.Put(r, "flash", "You've been loggout successfully!")
+
+	http.Redirect(w, r, "/", 303)
 }
