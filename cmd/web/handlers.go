9f9727d516095b7142e17bf49442eaef655a503c
faizisyellow <muhamatfaissalm@gmail.com>
2025-03-21 18:03:08 +0700

    validated email that already exist, complete signup handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -129,8 +129,26 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Otherwise send a placeholder response (for now!).
-	fmt.Fprintln(w, "Create a new user...")
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	// if the email already exist
+	// add an error message to the form and re-display it.
+	if err == models.ErrDuplicateEmail {
+		form.Errors.Add("email", "Address is already in use")
+		app.render(w, r, "signup.page.tmpl", &templateData{
+			Form: form,
+		})
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// Otherwise add a confirmation flash message to the session confirming that
+	// their signup worked and asking them to log in.
+	app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+
+	// And redirect the user to the login page.
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -2,8 +2,11 @@ package mysql
 
 import (
 	"database/sql"
+	"strings"
 
 	"faizisyellow.com/snippetbox/pkg/models"
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserModel struct {
@@ -12,7 +15,27 @@ type UserModel struct {
 
 // We'll use the Insert method to add a new record to the users table.
 func (m *UserModel) Insert(name, email, password string) error {
-	return nil
+
+	// Create a bcrypt hash of the plain-text password.
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES(?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+
+	if err != nil {
+		if mysqlError, ok := err.(*mysql.MySQLError); ok {
+			if mysqlError.Number == 1062 && strings.Contains(mysqlError.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+	}
+
+	return err
 }
 
 // We'll use the Authenticate method to verify whether a user exists with
